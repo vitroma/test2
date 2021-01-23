95a9cc86eb2e54589fb5171cd6e3a787d0463d21
vitroma <46964930+vitroma@users.noreply.github.com>
2021-01-23 22:11:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// VERSION: 8
+// VERSION: 9
 // 1. Save the files to the user's device
 // The "install" event is called when the ServiceWorker starts up.
 // All ServiceWorker code must be inside events.
