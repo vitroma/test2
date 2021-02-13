64d8706e624e08c8fe0f68e34ed51d3f3f605a93
vitroma <46964930+vitroma@users.noreply.github.com>
2021-02-13 16:34:56 +0100

    1

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 let ru_word;
 let de_word;
+let wordNumbers;
 
 
     function getRandomInt(max) {
       return Math.floor(Math.random() * Math.floor(max));
     }
 
+    function pageLoad(){
+      wordNumbers = dict.length; 
+      next();
 
-    function next(){
-      let l_len = dict.length;  
-      let l_cur_idx =  getRandomInt(l_len);
-      document.getElementById("ru").value = dict[l_cur_idx][0];
-      ru_word = dict[l_cur_idx][0];
-      de_word = dict[l_cur_idx][1];
+    }
+
+    function clearDeInput(){
       document.getElementById("de_input").value = "";
       document.getElementById("de_lable").classList.replace("w3-text-green", "w3-text-dark-grey");
       document.getElementById("de_lable").classList.replace("w3-text-red", "w3-text-dark-grey");
-   
 
+    }
+
+    function next(){
+      let l_cur_idx =  getRandomInt(wordNumbers); 
+      document.getElementById("ru").value = dict[l_cur_idx][0];
+      ru_word = dict[l_cur_idx][0];
+      de_word = dict[l_cur_idx][1];
+      clearDeInput();
       }
 
+    function onenter(e){
+      if(e.keyCode == 13) {
+        //document.getElementById("buttonCheck").focus();
+        //document.getElementById("buttonCheck").click();
+
+     }
+
+    }
 
     function check(){
       let cur_de_word = document.getElementById("de_input").value;
